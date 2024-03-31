a9d1f875eed9eec9c49fe37815ab523fb97d6a1c
kngatineau <kngatineau@outlook.com>
2024-03-31 15:50:32 -0600

    Remove ssh key pair management
    I initially set up Terraform to create an SSH keypair for my EC2 instance, and it worked.. but what good is a private key when the value is visible in the TF state file? :facepalm:
    
    Removed the logic to create the key pair and will manage these keys outside of Terraform.

--- tf/main.tf
+++ tf/main.tf
@@ -16,27 +16,28 @@ provider "aws" {
 resource "aws_instance" "ec2_ubuntu_instance" {
   ami           = var.ubuntu_ami
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.ssh_key.key_name
 
   tags = {
     Name = "katies_free_tier_instance"
   }
-}
-
-resource "tls_private_key" "ssh_private_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "ssh_key" {
-  key_name   = var.key_name
-  public_key = tls_private_key.ssh_private_key.public_key_openssh
-}
-
-resource "local_sensitive_file" "ssh_pem" {
-  count                = var.gen_pem_file ? 1 : 0
-  filename             = pathexpand("~/.ssh/ssh.pem")
-  file_permission      = "600"
-  directory_permission = "700"
-  content              = tls_private_key.ssh_private_key.private_key_pem
-}
+} 
+# just because you *can* generate SSH keys in Terraform doesn't mean you should. 
+# this commented code technically worked but having the key in the state file isn't ideal.
+#
+# resource "tls_private_key" "ssh_private_key" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
+#
+# resource "aws_key_pair" "ssh_key" {
+#   key_name   = var.key_name
+#   public_key = tls_private_key.ssh_private_key.public_key_openssh
+# }
+#
+# resource "local_sensitive_file" "ssh_pem" {
+#   count                = var.gen_pem_file ? 1 : 0
+#   filename             = pathexpand("~/.ssh/ssh.pem")
+#   file_permission      = "600"
+#   directory_permission = "700"
+#   content              = tls_private_key.ssh_private_key.private_key_pem
+# }
